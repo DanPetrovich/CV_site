7886c3c45f22930f6d7d062c515e34936e545436
Petrovich Danya <petrovichdanya@gmail.com>
2022-12-02 22:04:46 +0300

    add requirments

--- resume/resume/urls.py
+++ resume/resume/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from resume import settings
+from . import settings
 from resume_maker.views import *
 
 urlpatterns = [
